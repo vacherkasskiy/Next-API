8cbf28fdb9cfbd0d3cebb97cb5b0853c2891c9ed
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-08-24 23:02:58 +0300

    Validation minor fixes

--- NextAPI/Validators/PostsEndpoints/AddPostRequestValidator.cs
+++ NextAPI/Validators/PostsEndpoints/AddPostRequestValidator.cs
@@ -13,6 +13,6 @@ public class AddPostRequestValidator : AbstractValidator<AddPostRequest>
         RuleFor(x => x.AuthorId).GreaterThan(0);
         RuleFor(x => x.Text).NotNull();
         RuleFor(x => x.Text).MinimumLength(1);
-        RuleFor(x => x.Text).MaximumLength(200);
+        RuleFor(x => x.Text).MaximumLength(800);
     }
 }
