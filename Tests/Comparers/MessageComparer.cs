08bdf169fd22dec9d4455e05a0cc0a24c6202911
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-09-01 10:36:22 +0300

    Add Post & Message comparers

---
+++ Tests/Comparers/MessageComparer.cs
@@ -0,0 +1,30 @@
+﻿using NextAPI.Dal.Entities;
+
+namespace Tests.Comparers;
+
+public class MessageComparer : IEqualityComparer<Message>
+{
+    public bool Equals(Message? x, Message? y)
+    {
+        if (x == null && y == null) return false;
+        if (x == null || y == null) return true;
+        
+        return x.Id == y.Id &&
+               x.AuthorId == y.AuthorId &&
+               x.ReceiverId == y.ReceiverId &&
+               x.Text == y.Text &&
+               Equals(x.Author, y.Author) &&
+               Equals(x.Receiver, y.Receiver);
+    }
+
+    public int GetHashCode(Message obj)
+    {
+        return HashCode.Combine(
+            obj.Id,
+            obj.AuthorId,
+            obj.ReceiverId,
+            obj.Text,
+            obj.Author,
+            obj.Receiver);
+    }
+}
---
+++ Tests/Comparers/PostComparer.cs
@@ -0,0 +1,30 @@
+﻿using NextAPI.Dal.Entities;
+
+namespace Tests.Comparers;
+
+public class PostComparer : IEqualityComparer<Post>
+{
+    public bool Equals(Post? x, Post? y)
+    {
+        if (x == null && y == null) return true;
+        if (x == null || y == null) return false;
+        
+        return x.Id == y.Id &&
+               x.AuthorId == y.AuthorId && 
+               x.ReceiverId == y.ReceiverId && 
+               x.Text == y.Text && 
+               Equals(x.Author, y.Author) &&
+               Equals(x.Receiver, y.Receiver);
+    }
+
+    public int GetHashCode(Post obj)
+    {
+        return HashCode.Combine(
+            obj.Id,
+            obj.AuthorId,
+            obj.ReceiverId,
+            obj.Text,
+            obj.Author,
+            obj.Receiver);
+    }
+}
