87bed72ed5a6c0b2f87dac9b568b75866679b2f4
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-09-05 00:40:01 +0300

    Cookies are working inside server boundaries

---
+++ NextAPI/Bll/Services/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using NextAPI.Bll.Services.Interfaces;
+using NextAPI.Dal.Entities;
+using NextAPI.Dal.Repositories.Interfaces;
+
+namespace NextAPI.Bll.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly IBaseRepository<User> _repository;
+
+    public AuthService(IBaseRepository<User> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Register(User user)
+    {
+        var sameEmailUsers = (await _repository
+                .GetAll())
+            .Where(x => x.Email == user.Email)
+            .ToArray();
+
+        if (sameEmailUsers.Length > 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        await _repository.Add(user);
+    }
+
+    public async Task<ClaimsPrincipal> Login(string email, string password)
+    {
+        var user = (await _repository
+            .GetAll()).Single(x => x.Email == email);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+        
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var principal = new ClaimsPrincipal(identity);
+
+        return principal;
+    }
+
+    public Task Logout(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ NextAPI/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using NextAPI.Bll.Services.Interfaces;
+using NextAPI.Dal.Entities;
+using NextAPI.Requests.Auth;
+
+namespace NextAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _service;
+
+    public AuthController(IAuthService service)
+    {
+        _service = service;
+    }
+    
+    [HttpPost]
+    [Route("/auth/register")]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        try
+        {
+            await _service.Register(new User
+            {
+                Name = request.Name,
+                Username = request.Username,
+                Email = request.Email
+            });
+
+            return Ok();
+        }
+        catch
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpPost]
+    [Route("/auth/login")]
+    public async Task<IActionResult> Login(LoginRequest request)
+    {
+        var principal = await _service.Login(request.Email, request.Password);
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            principal);
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("/auth/get_current")]
+    public IActionResult GetCurrent()
+    {
+        if (User.Identity!.IsAuthenticated)
+        {
+            return Ok(User.FindFirstValue(ClaimTypes.Email));
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+}
